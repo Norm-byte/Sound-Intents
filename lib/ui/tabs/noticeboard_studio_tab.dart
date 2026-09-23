531840a30ee1b845430b61f098bbae6fb19ec947
Norman Smith <normansmith@Normans-MacBook-Pro.local>
2026-09-23 20:09:37 +0100

    fix(v69): align Learn More viewer with user app

--- lib/ui/tabs/noticeboard_studio_tab.dart
+++ lib/ui/tabs/noticeboard_studio_tab.dart
@@ -311,10 +311,10 @@ class _NoticeboardStudioTabState extends State<NoticeboardStudioTab> {
     if (isYoutube) {
       final id = _youtubeId(url);
       if (id != null) {
-        registerYoutubeViewFactory(viewId, 'https://www.youtube.com/embed/$id?autoplay=0&playsinline=1&rel=0');
+        registerYoutubeViewFactory(viewId, 'https://www.youtube.com/embed/$id?autoplay=1&mute=1&controls=0&playsinline=1&rel=0&modestbranding=1', autoPlay: true);
       }
     } else if (isPdf) {
-      registerPdfCanvasViewFactory(viewId, url);
+      registerPdfObjectViewFactory(viewId, '$url#toolbar=0&navpanes=0&scrollbar=0');
     } else if (!isImage && !isVideo) {
       registerPdfViewFactory(viewId, url);
     }
